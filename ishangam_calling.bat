30cb1e1bd4a80f07b768cb5508e615da02439736
Vishal Naik <vishal.naik@mercedes-benz.com>
2025-09-28 13:01:51 +0530

    bat to pull first

--- ishangam_calling.bat
+++
@@ -1,9 +0,0 @@
-rem This has to be set once in the begenning of setup
-set AIVEN_PASS=
-
-rem Get session ID from santosha. this has to be set for every run.
-set SESSION_ID=
-
-ishangam_campaign.exe --campaign --caller --loop 60 --session-id --config ie_wave_lead_nurturing.config
-ishangam_campaign.exe --campaign --caller --loop 90 --session-id --config ie_wave_lead_nurturing.config
-pause
