a402bf6dd8b08c18440289f8a429c93f441af67c
Vishal Naik <vishal.naik@mercedes-benz.com>
2025-09-28 12:56:39 +0530

    config file

--- ishangam_calling.bat
+++ ishangam_calling.bat
@@ -1,2 +1,5 @@
-ishangam_campaign.exe --campaign --caller --loop 60
+set AIVEN_PASS=
+set SESSION_ID=
+ishangam_campaign.exe --campaign --caller --loop 60 --session-id --config ie_wave_lead_nurturing.config
+ishangam_campaign.exe --campaign --caller --loop 90 --session-id --config ie_wave_lead_nurturing.config
 pause
